9564b0afee2f9accd35c0f19d6e1dc9d0402d1c3
cdonoff <cdonoff@ualberta.ca>
2020-02-04 11:00:12 -0800

    Updating route test function:

--- tests/testthat/test-driveBC_map_route.R
+++ tests/testthat/test-driveBC_map_route.R
@@ -1,6 +1,6 @@
 test_that("We are able to return a leaflet map ", {
   expect_error(driveBC_map_route("New York"))
-  expect_length(driveBC_map_major("Vancouver"),4)
-  expect_length(driveBC_map_major("Blind Bay","Vernon"),4)
+  expect_length(driveBC_map_major("Vancouver"),2)
+  expect_length(driveBC_map_major("Blind Bay","Vernon"),2)
   expect_error(driveBC_map_major("Vancouver",2))
 })
